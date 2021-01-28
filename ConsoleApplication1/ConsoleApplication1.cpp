345baeb81485ca29d314ab16ed36b9ce382e3a16
AnnaGawrysiak <anna.m.gawrysiak@gmail.com>
2021-01-28 16:13:51 +0100

    Add run() method

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,125 +1,14 @@
 ﻿#include "Caesar.h"
+#include "test.h"
 #include <iostream>
 #include <string>
 
-bool test1()
-{
-	std::string szyfr;
-	szyfr = "abc";
-	std::string oczekiwany_wynik = "def";
-
-	Caesar test;
-
-	std::string wynik = test.encrypt(szyfr, 3);
-	
-
-	if (wynik == oczekiwany_wynik)
-		return true;
-
-	return false;
-}
-/*
-bool test2() 
-{
-	std::string szyfr;
-	szyfr = "xyz"; // skrajnosci
-	std::string oczekiwany_wynik = "abc";
-
-	std::string  wynik = encrypt(szyfr, 3);
-
-	if (wynik == oczekiwany_wynik)
-		return true;
-
-	return false;
-}
-
-bool test3()
-{
-	std::string szyfr;
-	szyfr = "ABc"; 
-	std::string oczekiwany_wynik = "DEf";
-
-	std::string  wynik = encrypt(szyfr, 3);
-
-	if (wynik == oczekiwany_wynik)
-		return true;
-
-	return false;
-}
-
-bool test4()
-{
-	std::string szyfr;
-	szyfr = "XYZ";
-	std::string oczekiwany_wynik = "ABC";
-
-	std::string  wynik = encrypt(szyfr, 3);
-
-	if (wynik == oczekiwany_wynik)
-		return true;
-
-	return false;
-}
-
-bool test5()
-{
-	std::string szyfr;
-	szyfr = "abc";
-	std::string oczekiwany_wynik = "def";
-
-	std::string  wynik = encrypt(szyfr, 29);
-
-	if (wynik == oczekiwany_wynik)
-		return true;
-
-	return false;
-}
-
-bool test6()
-{
-	std::string szyfr;
-	szyfr = "def";
-	std::string oczekiwany_wynik = "abc";
-
-	std::string  wynik = encrypt(szyfr, -3);
-
-	if (wynik == oczekiwany_wynik)
-		return true;
-
-	return false;
-}
-
-bool test7()
-{
-	std::string szyfr;
-	szyfr = "abc";
-	std::string oczekiwany_wynik = "xyz";
-
-	std::string  wynik = encrypt(szyfr, -29);
-
-	if (wynik == oczekiwany_wynik)
-		return true;
-
-	return false;
-}
-
-bool test8()
-{
-	std::string szyfr;
-	szyfr = "ABC";
-	std::string oczekiwany_wynik = "XYZ";
-
-	std::string  wynik = encrypt(szyfr, -29);
-
-	if (wynik == oczekiwany_wynik)
-		return true;
-
-	return false;
-}
-*/
 int main()
 {
-	
+	test* testowa = test::get_instance();
+	testowa->run(); // testy maja sie same tworzyc, sprawdzac i wyczyscic. jest enkapsulacja.
+
+
 	std::string text;
 
 	char task;
@@ -168,47 +57,6 @@ int main()
 
 } while(!(task == 'Q'));
 
-	if (test1())
-		std::cout << "Test 1: Sukces! " << std::endl;
-	else
-		std::cout << "Test 1: Porazka " << std::endl;
-
-	/*
-	if (test2())
-		std::cout << "Test 2: Sukces! " << std::endl;
-	else
-		std::cout << "Test 2: Porazka " << std::endl;
-
-	if (test3())
-		std::cout << "Test 3: Sukces! " << std::endl;
-	else
-		std::cout << "Test 3: Porazka " << std::endl;
-
-	if (test4())
-		std::cout << "Test 4: Sukces! " << std::endl;
-	else
-		std::cout << "Test 4: Porazka " << std::endl;
-
-	if (test5())
-		std::cout << "Test 5: Sukces! " << std::endl;
-	else
-		std::cout << "Test 5: Porazka " << std::endl;
-
-	if (test6())
-		std::cout << "Test 6: Sukces! " << std::endl;
-	else
-		std::cout << "Test 6: Porazka " << std::endl;
-
-	if (test7())
-		std::cout << "Test 7: Sukces! " << std::endl;
-	else
-		std::cout << "Test 7: Porazka " << std::endl;
-
-	if (test8())
-		std::cout << "Test 8: Sukces! " << std::endl;
-	else
-		std::cout << "Test 8: Porazka " << std::endl;
-	*/
 	return 0;
 
 }
